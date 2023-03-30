01e7f0d38f78d2831fc030bdb75809afcb42e679
wisdomjohnson <wisdomjohnson207@gmail.com>
2023-03-30 08:28:35 +0100

    changes to home and footer, started article.html

--- main.js
+++ main.js
@@ -71,37 +71,108 @@ window.addEventListener("scroll", () => {
     .classList.toggle("nav-sticky", window.scrollY > 10);
 });
 
-//javascript for responsive navigation sidebar menu
-let menu = document.querySelector("#main-nav.nav-container");
-let menuBtn = document.querySelector("button.open-nav-menu");
-// const closeBtn = document.querySelector(".close-nav-menu");
+// preloader
 
-closeBtn.onclick = function () {
-  menu.classList.add("open-menu");
-  menu.classList.toggle("open-menu");
+window.addEventListener("load", function () {
+  const preloader = document.getElementById("preloader");
+  setTimeout(function () {
+    preloader.style.display = "none";
+  }, 1500);
+});
 
-  if (menu.classList.contains("open-menu")) {
-    menuBtn.src = "./img/images (1).png";
-  }
+// navbar menu toggle
+
+const toggle = document.getElementById("open-nav");
+const closeBtn = document.getElementById("close-nav");
+const navmenu = document.querySelector("#main-nav .nav-container");
+
+// Toggle nav
+toggle.addEventListener("click", () => {
+  // shows nav
+  navmenu.style.display = "block";
+  // shows close button
+  closeBtn.style.display = "inline-block";
+  // hides menu button
+  toggle.style.display = "none";
+});
+closeBtn.addEventListener("click", () => {
+  // hides nav
+  navmenu.style.display = "none";
+  // hides close button
+  closeBtn.style.display = "none";
+  // shows menu button
+  toggle.style.display = "inline-block";
+});
+
+// // Show modal
+// open.addEventListener("click", () => modal.classList.add("show-modal"));
+
+// // Hide modal
+// close.addEventListener("click", () => modal.classList.remove("show-modal"));
+
+// // Hide modal on outside click
+// window.addEventListener("click", (e) =>
+//   e.target == modal ? modal.classList.remove("show-modal") : false
+// );
+
+// animate on scroll
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+const hidden = document.querySelectorAll(".hidden");
+hidden.forEach((el) => observer.observe(el));
+
+// JavaScript code to fade in boxes from the left as they become visible in the viewport
+const boxes = document.querySelectorAll(".boxes");
+
+const options = {
+  rootMargin: "0px",
+  threshold: 0.5,
 };
+const boxObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("boxIn");
+    } else {
+      entry.target.classList.remove("boxIn");
+    }
+  });
+}, options);
+const hide = document.querySelectorAll(".boxOut");
+hide.forEach((el) => observer.observe(el));
+function showdate() {
+  // Get the current date and time
+  let now = new Date();
 
-// preloader function
-// jQuery(window).load(function () {
-// makes sure the whole site is loaded
-// $("#status").fadeOut(); // will first fade out the loading animation
-// $("#preloader").delay(100).fadeOut("slow"); // will fade out the white DIV that covers the website.
-//   $("body").delay(100).css({
-//     overflow: "visible",
-//   });
-// });
+  // Get the user's timezone offset from UTC in minutes
+  let timezoneOffset = now.getTimezoneOffset();
 
-function myPreloader() {
-  document.querySelector(".preloader").classList.add("fade-out");
+  // Convert the timezone offset to hours and adjust the date and time accordingly
+  now.setHours(now.getHours() + timezoneOffset);
+
+  // Format the date and time as a string
+  let dateAndTime = now.toLocaleString();
+
+  // Get a reference to the HTML element where you want to display the date and time
+  let dateTimeElement = document.getElementById("datetime");
+
+  // Set the text content of the HTML element to the formatted date and time string
+  dateTimeElement.textContent = ` ${dateAndTime}`;
+  // chat gpt made a mistake here, it should have added the last line above as a template string
 }
-function fadeOut() {
-  setInterval(myPreloader, 300);
-}
-window.onload = fadeOut;
-// window.addEventListener("load", fadeOutEffect);
-// console.log(fadeOutEffect);
-// console.log(myPreloader);
+
+setInterval(showdate, 1);
+
+// slider animation
+// const counter= 1;
+// setInterval(function){
+//   document.getElementById()
+// }
