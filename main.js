20b2a1d62fb193f3beee9788034163053bb74c82
wisdomjohnson <wisdomjohnson207@gmail.com>
2023-04-22 10:28:44 +0100

    index.html problem solved, changes in slider

--- main.js
+++ main.js
@@ -81,6 +81,36 @@ window.addEventListener("load", function () {
     preloader.style.display = "none";
   }, 1500);
 });
+//  date and time
+
+function showdate() {
+  var date = Date();
+  var dateTimeElement = document.querySelector("#datetime");
+  dateTimeElement.textContent = `${date}`;
+}
+// Initiate On DOM Load
+document.addEventListener("DOMContentLoaded", showdate);
+setInterval(showdate, 1);
+// animate on scroll
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+      entry.target.classList.add("show2");
+      observer.unobserve(entry.target);
+    } else {
+      entry.target.classList.remove("show");
+      entry.target.classList.remove("show2");
+    }
+  });
+}, {});
+const hidden = document.querySelectorAll(".hidden");
+const hidden2 = document.querySelectorAll(".hidden2");
+const video = document.getElementById("stream-video");
+hidden.forEach((el) => observer.observe(el));
+hidden2.forEach((el) => observer.observe(el));
 
 // navbar menu and list menu toggle
 
@@ -96,7 +126,7 @@ toggle.addEventListener("click", () => {
   // shows nav
   navmenu.style.display = "block";
   // shows close button
-  closeBtn.style.display = "inline-block";
+  closeBtn.style.display = "inline";
   // hides menu button
   toggle.style.display = "none";
 });
@@ -106,7 +136,7 @@ closeBtn.addEventListener("click", () => {
   // hides close button
   closeBtn.style.display = "none";
   // shows menu button
-  toggle.style.display = "inline-block";
+  toggle.style.display = "";
 });
 listmenu.addEventListener("click", () => {
   // shows topics-box-dropdown
@@ -124,88 +154,100 @@ hidemenu.addEventListener("click", () => {
   // hides hide-menu icon
   hidemenu.style.display = "none";
 });
-// this represents the search box that will pop on the screen with opacity so that it fades in and out- it's from brad's project
-// // Show modal
-// open.addEventListener("click", () => modal.classList.add("show-modal"));
-
-// // Hide modal
-// close.addEventListener("click", () => modal.classList.remove("show-modal"));
-
-// // Hide modal on outside click
-// window.addEventListener("click", (e) =>
-//   e.target == modal ? modal.classList.remove("show-modal") : false
-// );
-
-// animate on scroll
-
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry);
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-      entry.target.classList.add("show2");
-    } else {
-      entry.target.classList.remove("show");
-      entry.target.classList.remove("show2");
-    }
-  });
-}, {});
-const hidden = document.querySelectorAll(".hidden");
-const hidden2 = document.querySelectorAll(".hidden2");
-const video = document.getElementById("stream-video");
-hidden.forEach((el) => observer.observe(el));
-hidden2.forEach((el) => observer.observe(el));
-// video.forEach((el) => observer.observe(el));
-
-//  date and time
-
-function showdate() {
-  var date = Date();
-  var dateTimeElement = document.getElementById("datetime");
-  dateTimeElement.textContent = `${date}`;
+function handleChange(el) {
+  if (window.scrollY > 15) {
+    document.querySelector("#main-nav").style.display = "none";
+    document.querySelector("#topic-container").style.top = ".5rem";
+  } else {
+    document.querySelector("#main-nav").style.display = "";
+    document.querySelector("#topic-container").style.top = "3.5rem";
+  }
 }
-// Initiate On DOM Load
-document.addEventListener("DOMContentLoaded", showdate);
-setInterval(showdate, 1);
 
 // slider
-const slider = document.getElementById("slides");
-const leftBtn = document.getElementById("left");
-const rightBtn = document.getElementById("right");
-const firstslide = document.querySelector(".first");
-const secondslide = document.querySelector(".second");
-const thirdslide = document.querySelector(".third");
 
 const img = document.querySelectorAll(".slide img");
 
-let idx = 0;
+var slides = document.querySelectorAll(".slide");
+var btns = document.querySelectorAll(".btn");
+let currentSlide = 1;
 
-let interval = setInterval(run, 4000);
+// Javascript for image slider manual navigation
+var manualNav = function (manual) {
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
 
-function run() {
-  idx++;
-  changeImage();
-}
+    btns.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+  });
 
-function changeImage() {
-  if (idx > img.length - 1) {
-    idx = 0;
-  } else if (idx < 0) {
-    idx = img.length - 1;
-  }
-  firstslide.style.marginLeft = "-20%";
-  setTimeout(() => {
-    secondslide.style.marginLeft = "-20%";
-  }, 2000);
-}
+  slides[manual].classList.add("active");
+  btns[manual].classList.add("active");
+};
 
-rightBtn.addEventListener("click", () => {
-  firstslide.style.marginLeft = " -20%";
+btns.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    manualNav(i);
+    currentSlide = i;
+  });
 });
+// Javascript for image slider autoplay navigation
+var repeat = function (activeClass) {
+  let active = document.getElementsByClassName("active");
+  let i = 1;
 
-leftBtn.addEventListener("click", () => {
-  secondslide.style.marginLeft = " -20%";
-});
+  var repeater = () => {
+    setTimeout(function () {
+      [...active].forEach((activeSlide) => {
+        activeSlide.classList.remove("active");
+      });
+
+      slides[i].classList.add("active");
+      btns[i].classList.add("active");
+      i++;
+
+      if (slides.length == i) {
+        i = 0;
+      }
+      if (i >= slides.length) {
+        return;
+      }
+      repeater();
+    }, 4000);
+  };
+  repeater();
+};
+repeat();
+
+// let idx = 0;
+
+// let interval = setInterval(run, 4000);
+
+// function run() {
+//   idx++;
+//   changeImage();
+// }
+
+// function changeImage() {
+//   if (idx > img.length - 1) {
+//     idx = 0;
+//   } else if (idx < 0) {
+//     idx = img.length - 1;
+//   }
+//   firstslide.style.marginLeft = "-20%";
+//   setTimeout(() => {
+//     secondslide.style.marginLeft = "-20%";
+//   }, 4000);
+// }
+
+// rightBtn.addEventListener("click", () => {
+//   firstslide.style.marginLeft = " -20%";
+// });
+
+// leftBtn.addEventListener("click", () => {
+//   secondslide.style.marginLeft = " -20%";
+// });
 
 // Smooth Scrolling
 $(".nav-container a, #topic-container a").on("click", function (event) {
@@ -221,4 +263,8 @@ $(".nav-container a, #topic-container a").on("click", function (event) {
       800
     );
   }
+});
+$("a[href='#top']").click(function () {
+  $("html, body").animate({ scrollTop: 0 }, "slow");
+  return false;
 });
