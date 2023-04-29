945a6ee75868e465336646ae0d43d7d07037a55f
wisdomjohnson <wisdomjohnson207@gmail.com>
2023-04-29 04:37:54 +0100

    done with the contact page

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-function showdate() {
-  var e = Date();
-  document.querySelector("#datetime").textContent = `${e}`;
-}
-document.addEventListener("DOMContentLoaded", showdate),
-  setInterval(showdate, 1);
 // preloader
 window.addEventListener("load", function () {
   const e = document.getElementById("preloader");
@@ -11,6 +5,110 @@ window.addEventListener("load", function () {
     e.style.display = "none";
   }, 1500);
 });
+function showdate() {
+  // code to execute if index.html is being displayed
+  var now = new Date();
+  var days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  var months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  var dayOfWeek = days[now.getDay()];
+  var dayOfMonth = now.getDate();
+  var month = months[now.getMonth()];
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+  var seconds = now.getSeconds();
+  var timeText = `${hours}:${minutes}:${seconds}`;
+  var dateText = `${dayOfWeek}, ${dayOfMonth} ${month} ${timeText}`;
+  document.querySelector("#datetime").textContent = dateText;
+}
+document.addEventListener("DOMContentLoaded", showdate),
+  setInterval(showdate, 1);
+class TypeWriter {
+  constructor(txtElement, words, wait = 3000) {
+    this.txtElement = txtElement;
+    this.words = words;
+    this.txt = "";
+    this.wordIndex = 0;
+    this.wait = parseInt(wait, 10);
+    this.type();
+    this.isDeleting = false;
+  }
+
+  type() {
+    // Current index of word
+    const current = this.wordIndex % this.words.length;
+    // Get full text of current word
+    const fullTxt = this.words[current];
+
+    // Check if deleting
+    if (this.isDeleting) {
+      // Remove char
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      // Add char
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    // Insert txt into element
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+    // Initial Type Speed
+    let typeSpeed = 200;
+
+    if (this.isDeleting) {
+      typeSpeed /= 2;
+    }
+
+    // If word is complete
+    if (!this.isDeleting && this.txt === fullTxt) {
+      // Make pause at end
+      typeSpeed = this.wait;
+      // Set delete to true
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === "") {
+      this.isDeleting = false;
+      // Move to next word
+      this.wordIndex++;
+      // Pause before start typing
+      typeSpeed = 500;
+    }
+
+    setTimeout(() => this.type(), typeSpeed);
+  }
+}
+
+// Init On DOM Load
+document.addEventListener("DOMContentLoaded", init);
+
+// Init App
+function init() {
+  const txtElement = document.querySelector(".header-text");
+  const words = JSON.parse(txtElement.getAttribute("data-words"));
+  const wait = txtElement.getAttribute("data-wait");
+  // Init TypeWriter
+  new TypeWriter(txtElement, words, wait);
+}
+// change in background of  navbar on scroll
 document.addEventListener(
   "DOMContentLoaded",
   window.addEventListener("scroll", function () {
