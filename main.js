c991c1b77b1fe7a5b54b38362857ceb1b64af225
wisdomjohnson <wisdomjohnson207@gmail.com>
2023-05-02 04:20:40 +0100

    contact form has functionality

--- main.js
+++ main.js
@@ -5,109 +5,118 @@ window.addEventListener("load", function () {
     e.style.display = "none";
   }, 1500);
 });
-function showdate() {
-  // code to execute if index.html is being displayed
-  var now = new Date();
-  var days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  var months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  var dayOfWeek = days[now.getDay()];
-  var dayOfMonth = now.getDate();
-  var month = months[now.getMonth()];
-  var hours = now.getHours();
-  var minutes = now.getMinutes();
-  var seconds = now.getSeconds();
-  var timeText = `${hours}:${minutes}:${seconds}`;
-  var dateText = `${dayOfWeek}, ${dayOfMonth} ${month} ${timeText}`;
-  document.querySelector("#datetime").textContent = dateText;
-}
-document.addEventListener("DOMContentLoaded", showdate),
-  setInterval(showdate, 1);
-class TypeWriter {
-  constructor(txtElement, words, wait = 3000) {
-    this.txtElement = txtElement;
-    this.words = words;
-    this.txt = "";
-    this.wordIndex = 0;
-    this.wait = parseInt(wait, 10);
-    this.type();
-    this.isDeleting = false;
+if (window.location.pathname !== "/contact.html") {
+  function showdate() {
+    // code to execute if index.html is being displayed
+    var now = new Date();
+    var days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    var months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    var dayOfWeek = days[now.getDay()];
+    var dayOfMonth = now.getDate();
+    var month = months[now.getMonth()];
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
+    var timeText = `${hours}:${minutes}:${seconds}`;
+    var dateText = `${dayOfWeek}, ${dayOfMonth} ${month} ${timeText}`;
+    document.querySelector("#datetime").textContent = dateText;
   }
-
-  type() {
-    // Current index of word
-    const current = this.wordIndex % this.words.length;
-    // Get full text of current word
-    const fullTxt = this.words[current];
-
-    // Check if deleting
-    if (this.isDeleting) {
-      // Remove char
-      this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-      // Add char
-      this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
-
-    // Insert txt into element
-    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
-
-    // Initial Type Speed
-    let typeSpeed = 200;
-
-    if (this.isDeleting) {
-      typeSpeed /= 2;
-    }
-
-    // If word is complete
-    if (!this.isDeleting && this.txt === fullTxt) {
-      // Make pause at end
-      typeSpeed = this.wait;
-      // Set delete to true
-      this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === "") {
+  document.addEventListener("DOMContentLoaded", showdate),
+    setInterval(showdate, 1);
+} else {
+  console.log("no text content");
+}
+if (window.location.pathname === "/index.html") {
+  class TypeWriter {
+    constructor(txtElement, words, wait = 3000) {
+      this.txtElement = txtElement;
+      this.words = words;
+      this.txt = "";
+      this.wordIndex = 0;
+      this.wait = parseInt(wait, 10);
+      this.type();
       this.isDeleting = false;
-      // Move to next word
-      this.wordIndex++;
-      // Pause before start typing
-      typeSpeed = 500;
     }
 
-    setTimeout(() => this.type(), typeSpeed);
+    type() {
+      // Current index of word
+      const current = this.wordIndex % this.words.length;
+      // Get full text of current word
+      const fullTxt = this.words[current];
+
+      // Check if deleting
+      if (this.isDeleting) {
+        // Remove char
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+      } else {
+        // Add char
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+      }
+
+      // Insert txt into element
+      this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+      // Initial Type Speed
+      let typeSpeed = 200;
+
+      if (this.isDeleting) {
+        typeSpeed /= 2;
+      }
+
+      // If word is complete
+      if (!this.isDeleting && this.txt === fullTxt) {
+        // Make pause at end
+        typeSpeed = this.wait;
+        // Set delete to true
+        this.isDeleting = true;
+      } else if (this.isDeleting && this.txt === "") {
+        this.isDeleting = false;
+        // Move to next word
+        this.wordIndex++;
+        // Pause before start typing
+        typeSpeed = 500;
+      }
+
+      setTimeout(() => this.type(), typeSpeed);
+    }
   }
+
+  // Init On DOM Load
+  document.addEventListener("DOMContentLoaded", init);
+
+  // Init App
+  function init() {
+    const txtElement = document.querySelector(".header-text");
+    const words = JSON.parse(txtElement.getAttribute("data-words"));
+    const wait = txtElement.getAttribute("data-wait");
+    // Init TypeWriter
+    new TypeWriter(txtElement, words, wait);
+  }
+} else {
+  console.log("no available text content");
 }
 
-// Init On DOM Load
-document.addEventListener("DOMContentLoaded", init);
-
-// Init App
-function init() {
-  const txtElement = document.querySelector(".header-text");
-  const words = JSON.parse(txtElement.getAttribute("data-words"));
-  const wait = txtElement.getAttribute("data-wait");
-  // Init TypeWriter
-  new TypeWriter(txtElement, words, wait);
-}
 // change in background of  navbar on scroll
 document.addEventListener(
   "DOMContentLoaded",
@@ -117,6 +126,44 @@ document.addEventListener(
       : (document.querySelector("#main-nav").style.opacity = "1");
   })
 );
+// slides
+const img = document.querySelectorAll(".slide img");
+var slides = document.querySelectorAll(".slide"),
+  btns = document.querySelectorAll(".btn");
+let currentSlide = 1;
+var manualNav = function (e) {
+  slides.forEach((e) => {
+    e.classList.remove("active"),
+      btns.forEach((e) => {
+        e.classList.remove("active");
+      });
+  }),
+    slides[e].classList.add("active"),
+    btns[e].classList.add("active");
+};
+btns.forEach((e, t) => {
+  e.addEventListener("click", () => {
+    manualNav(t), (currentSlide = t);
+  });
+});
+var repeat = function (e) {
+  let t = document.getElementsByClassName("active"),
+    n = 1;
+  var s = () => {
+    setTimeout(function () {
+      [...t].forEach((e) => {
+        e.classList.remove("active");
+      }),
+        slides[n].classList.add("active"),
+        btns[n].classList.add("active"),
+        n++,
+        slides.length == n && (n = 0),
+        n >= slides.length || s();
+    }, 75e2);
+  };
+  s();
+};
+repeat();
 
 const observer = new IntersectionObserver((e) => {
     e.forEach((e) => {
@@ -199,41 +246,3 @@ $(".nav-container a, #topic-container a").on("click", function (e) {
 $("a[href='#top']").click(function () {
   return $("html, body").animate({ scrollTop: 0 }, "slow"), !1;
 });
-// slides
-const img = document.querySelectorAll(".slide img");
-var slides = document.querySelectorAll(".slide"),
-  btns = document.querySelectorAll(".btn");
-let currentSlide = 1;
-var manualNav = function (e) {
-  slides.forEach((e) => {
-    e.classList.remove("active"),
-      btns.forEach((e) => {
-        e.classList.remove("active");
-      });
-  }),
-    slides[e].classList.add("active"),
-    btns[e].classList.add("active");
-};
-btns.forEach((e, t) => {
-  e.addEventListener("click", () => {
-    manualNav(t), (currentSlide = t);
-  });
-});
-var repeat = function (e) {
-  let t = document.getElementsByClassName("active"),
-    n = 1;
-  var s = () => {
-    setTimeout(function () {
-      [...t].forEach((e) => {
-        e.classList.remove("active");
-      }),
-        slides[n].classList.add("active"),
-        btns[n].classList.add("active"),
-        n++,
-        slides.length == n && (n = 0),
-        n >= slides.length || s();
-    }, 4e3);
-  };
-  s();
-};
-repeat();
