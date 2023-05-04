e5913c6cb34a8d2cffdec2d24e82e2c52fc5d03a
wisdomjohnson <wisdomjohnson207@gmail.com>
2023-05-04 12:50:08 +0100

    INDEX PAGE ERROR FIXED

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ window.addEventListener("load", function () {
     e.style.display = "none";
   }, 1500);
 });
-if (window.location.pathname !== "/contact.html") {
+if (window.location.pathname !== "/news-site/contact.html") {
   function showdate() {
     // code to execute if index.html is being displayed
     var now = new Date();
@@ -47,7 +47,7 @@ if (window.location.pathname !== "/contact.html") {
 } else {
   console.log("no text content");
 }
-if (window.location.pathname === "/index.html") {
+if (window.location.pathname === "/news-site/index.html") {
   class TypeWriter {
     constructor(txtElement, words, wait = 3000) {
       this.txtElement = txtElement;
